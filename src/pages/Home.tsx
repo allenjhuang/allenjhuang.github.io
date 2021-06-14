a5f73923b1bd71bf8610eec9f8b2428dd6973ed3
Allen <allenjhuang92@gmail.com>
2021-06-13 19:09:29 -0500

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div className="Home container is-fluid">
+      <div className="columns">
+        <div className="column"></div>
+        <div className="column is-three-fifths">
+          <h3>Hi, I'm Allen.</h3>
+          <p>I'm interested in many things, but am currently most into automation, microservices, manga, video games, and dance.</p>
+        </div>
+        <div className="column"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
